964709ef080853a4b5a1fc4af7724baa91fe25a4
unknown <996315826@qq.com>
2019-06-04 18:26:32 +0800

    Initial commit

---
+++ opp.py
@@ -0,0 +1,5 @@
+# 写在前面的话
+from typing import Set
+
+s: Set[int] = {1,2,3,4,5}
+ print(i for i
---
+++ venv/01.py
@@ -0,0 +1,14 @@
+'''
+定义一个学生类，用来形容学生
+'''
+# 定义一个空的类
+class Student():
+
+    pass
+
+# 定义一个对象
+mingyue = Student()
+
+# 定义一个用来描述python的学生
+class PythonStudent():
+    name = None
